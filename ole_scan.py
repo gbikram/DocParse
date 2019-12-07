92d0a852576d2eb48efffbf4e0b973b5947c52ef
bikram <ghurabikram@gmail.com>
2019-12-07 23:42:21 +0000

    Add docx check, argument parsing and todo

--- ole_scan.py
+++ ole_scan.py
@@ -1,13 +1,15 @@
 from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XML, TYPE_MHTML
 from docx import Document
 from docx.opc.constants import RELATIONSHIP_TYPE as RT
+import sys
 
 # Uses olevba to analyze macros and get all IOCs from a Macro
 def getVbaIOCs(filename):
     vbaparser = VBA_Parser(filename)
     results = vbaparser.analyze_macros()
-    for kw_type, keyword, description in results:
-        print('type=%s - keyword=%s - description=%s' % (kw_type, keyword, description))
+    if(results != None):
+        for kw_type, keyword, description in results:
+            print('type=%s - keyword=%s - description=%s' % (kw_type, keyword, description))
     vbaparser.close()
 
 # Get URLs from Docx - constraint - only accepts docx
@@ -18,11 +20,16 @@ def getDocURLs(filename):
     for rel in rels:
         if rels[rel].reltype == RT.HYPERLINK:      
             print(rels[rel]._target)
-print()
-print("IOCs from Embedded Macro:")
-getVbaIOCs("mal-samples/__00FYI SHIPPING DOC.doc")
-print()
-print("=================================================")
-print()
-print("IOCs from Document Body")
-getDocURLs("mal-samples/addresses.docx")
+
+filename = sys.argv[1]
+if(filename.endswith(".docx")):
+    print()
+    print("IOCs from Embedded Macro:")
+    getVbaIOCs(filename)
+    print()
+    print("=================================================")
+    print()
+    print("IOCs from Document Body")
+    getDocURLs(filename)
+else:
+    print("Given file is not a DOCX!")
