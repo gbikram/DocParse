a25c33000e595cd4783b7394dd08f9b2e45a73e9
bikram <ghurabikram@gmail.com>
2020-03-18 20:08:24 -0700

    Add upload and process file functionality

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request
 import ole_scan
 from flask import render_template
 import requests
+import os
+
+UPLOAD_FOLDER = "./uploadedfiles"
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 # Generate Front-end
 def index():
     return render_template("index.html")
 
-#@app.route('/AnalyzeFile', methods = ["POST"])
-#def analyzeFile():
-#    if request.method == "POST":
+@app.route('/AnalyzeFile', methods = ["POST"])
+def analyzeFile():
+   if request.method == "POST":
+        print("POST DETECT")
         # Get file from client
-#        request.
+        file = request.files['file']
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename) 
+        file.save(filepath)    
+        print("FILE RETRIEVED")
         # Pass file for processing
+        ole_scan.startScan(filepath)
+        return "File Uploaded"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
