bb33ce75d6bfea59280107a8ef5270237d837976
bikram <ghurabikram@gmail.com>
2020-03-23 13:16:05 -0700

    Add eml docx doc macros

---
+++ templates/Macro.vue
@@ -0,0 +1,26 @@
+<template>
+  <table class="table" id="macro">
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Keyword</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="macroioc in macroiocs">
+        <td>${ macroioc.type }</td>
+        <td>${ macroioc.keyword }</td>
+        <td>${ macroioc.description }</td>
+      </tr>
+    </tbody>
+  <table>
+</template>
+
+<script>
+export default {
+ props: [
+   'macroIocs'
+   ]
+}
+</script>
