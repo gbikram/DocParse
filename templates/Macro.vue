3226de9ee1d06f8e3e6c2fcd3bcffc481aae7296
bikram <ghurabikram@gmail.com>
2020-03-23 16:27:24 -0700

    Update Todo

--- templates/Macro.vue
+++ templates/Macro.vue
@@ -1,26 +1,38 @@
 <template>
-  <table class="table" id="macro">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Keyword</th>
-        <th>Description</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="macroioc in macroiocs">
-        <td>${ macroioc.type }</td>
-        <td>${ macroioc.keyword }</td>
-        <td>${ macroioc.description }</td>
-      </tr>
-    </tbody>
-  <table>
+  <div class="modal" :class="{ 'is-active': viewMacro }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">IOCs from Embedded VBA</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <table class="table" id="macro">
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Keyword</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="macroioc in macroiocs">
+              <td>${ macroioc.type }</td>
+              <td>${ macroioc.keyword }</td>
+              <td>${ macroioc.description }</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success" v-on:click="viewMacro = false">Done</button>
+      </footer>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
- props: [
-   'macroIocs'
-   ]
-}
+  props: ["macroIocs"]
+};
 </script>
