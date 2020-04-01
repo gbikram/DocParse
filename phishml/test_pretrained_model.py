76e54bcbf0aaeca35e362103c9025c96f3f06193
bikram <ghurabikram@gmail.com>
2020-03-31 19:30:35 -0700

    Modify phishml script to use as function

--- phishml/test_pretrained_model.py
+++ phishml/test_pretrained_model.py
@@ -1,3 +1,5 @@
+# https://github.com/faizann24/phishytics-machine-learning-for-phishing
+
 # Basic libraries
 import os
 import io
@@ -19,65 +21,61 @@ How to run:
 python3 test_pretrained_model.py --tokenizer_folder pretrained_models --threshold 0.5 --model_dir pretrained_models --website_to_test https://www.google.com
 """
 
-# Parsing arguments
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--tokenizer_folder", type=str, default = "pretrained_models", help="Folder where tokenizer files have been placed")
-parser.add_argument("--threshold", type=float, default = 0.5, help="Which threshold to use for testing")
-parser.add_argument("--model_dir", type=str, default = "pretrained-models", help="Directory of trained models.")
-parser.add_argument("--website_to_test", type=str, default = "https://www.google.com", help="Website to test")
+def runPhishCheck(website):
+	tokenizerFolder = "pretrained_models"
+	savedModelDirectory = "pretrained_models"
+	threshold = 0.5
 
-args = parser.parse_args()
-tokenizerFolder = args.tokenizer_folder
-savedModelDirectory = args.model_dir
-websiteToTest = args.website_to_test
-threshold = args.threshold
+	# Loading files
+	# Load tokenization files
+	tokenizer = ByteLevelBPETokenizer(
+		tokenizerFolder + "/pretrained_Tokenizer-10000.tok-vocab.json",
+		tokenizerFolder + "/pretrained_Tokenizer-10000.tok-merges.txt",
+	)
+	tokenizerVocabSize = tokenizer.get_vocab_size()
 
-# Loading files
-# Load tokenization files
-tokenizer = ByteLevelBPETokenizer(
-    tokenizerFolder + "/pretrained_Tokenizer-10000.tok-vocab.json",
-    tokenizerFolder + "/pretrained_Tokenizer-10000.tok-merges.txt",
-)
-tokenizerVocabSize = tokenizer.get_vocab_size()
-print("Tokenizer files have been loaded and the vocab size is %d..." % tokenizerVocabSize)
+	# Load saved model
+	model = load(savedModelDirectory + "/pretrained-phishytics-model.joblib")
 
-# Load saved model
-model = load(savedModelDirectory + "/pretrained-phishytics-model.joblib")
-print("Model loaded...")
+	# Load document frequency dictionary
+	docDict = np.load(savedModelDirectory + "/document-frequency-dictionary.npy", allow_pickle=True).item()
 
-# Load document frequency dictionary
-docDict = np.load(savedModelDirectory + "/document-frequency-dictionary.npy", allow_pickle=True).item()
-print("Document frequency dictionary loaded...")
+	# Testing
+	try:
+		request = requests.get(website)
+		webpageHtml = str(request.text)
+		webpageHtml = webpageHtml.replace("\n", " ")
 
-# Testing
-print("Loading webpage...")
-try:
-	request = requests.get(websiteToTest)
-	webpageHtml = str(request.text)
-	webpageHtml = webpageHtml.replace("\n", " ")
+		# Convert text into feature vector
+		output = tokenizer.encode(webpageHtml)
+		outputDict = collections.Counter(output.ids)
+	except Exception as e:
+		print("**** Error loading the website ****")
+		print(e)
+		exit()
 
-	# Convert text into feature vector
-	output = tokenizer.encode(webpageHtml)
-	outputDict = collections.Counter(output.ids)
-except Exception as e:
-	print("**** Error loading the website ****")
-	print(e)
-	exit()
+	# Apply tfidf weighting
+	totalFilesUnderConsideration = 18500 # total number of documents/html files in our training data
+	array = [0] * tokenizerVocabSize
+	for item in outputDict:
+		if len(docDict[item]) > 0:
+			array[item] = (outputDict[item]) * (math.log10( totalFilesUnderConsideration / len(docDict[item] )))
 
-# Apply tfidf weighting
-totalFilesUnderConsideration = 18500 # total number of documents/html files in our training data
-array = [0] * tokenizerVocabSize
-for item in outputDict:
-	if len(docDict[item]) > 0:
-		array[item] = (outputDict[item]) * (math.log10( totalFilesUnderConsideration / len(docDict[item] )))
+	# Getting predictions
+	predictionProbability = model.predict_proba([array])[0][1]
+	# print("\n****************************\n--> Probability that the website is phishing: %.2f" % (predictionProbability * 100))
 
-# Getting predictions
-predictionProbability = model.predict_proba([array])[0][1]
-print("\n****************************\n--> Probability that the website is phishing: %.2f" % (predictionProbability * 100))
+	# Default is not phishing 
+	prediction = False
+	if predictionProbability > threshold:
+		prediction = True
+	print(prediction)
+	return prediction
+	# print("--> Based on your threshold of %.2f, this website is +++'%s'+++" % (threshold, prediction))
+	# print("****************************")
 
-prediction = "NOT PHISHING"
-if predictionProbability > threshold:
-	prediction = "PHISHING"
-print("--> Based on your threshold of %.2f, this website is +++'%s'+++" % (threshold, prediction))
-print("****************************")
+# Only for running the script directly like cli
+if __name__ == "__main__":
+    # execute only if run as a script
+    website = sys.argv[1]
+    runPhishCheck(website)
